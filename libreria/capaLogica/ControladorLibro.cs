178ff1552eda868e3b77496f480906192938fec4
gastonferron <bruceferr@gmail.com>
2023-09-02 21:10:10 -0300

    Creacion del ControladorSocio

--- libreria/capaLogica/ControladorLibro.cs
+++ libreria/capaLogica/ControladorLibro.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using capaDatos;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,8 @@ namespace capaLogica
 {
     class ControladorLibro
     {
+
+        
+
     }
 }
--- libreria/capaLogica/ControladorPrestamo.cs
+++ libreria/capaLogica/ControladorPrestamo.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using capaDatos;
+using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,46 @@ namespace capaLogica
 {
     class ControladorPrestamo
     {
+        public static void Crear(int socio, int libro, string fechaPrestamo, string fechaDevolucion)
+        {
+            ModelPrestamo prestamo = new ModelPrestamo();
+            prestamo.socio = socio;
+            prestamo.libro = libro;
+            prestamo.fechaPrestamo = fechaPrestamo;
+            prestamo.fechaDevolucion = fechaDevolucion;
+            prestamo.Save();
+        }
+
+        public static DataTable Obtener()
+        {
+            ModelPrestamo PrestamoTableModel = new ModelPrestamo();
+            List<ModelPrestamo> prestamos = PrestamoTableModel.obtenerPrestamos();
+
+            DataTable table = new DataTable();
+            table.Columns.Add("id", typeof(int));
+            table.Columns.Add("socio", typeof(int));
+            table.Columns.Add("libro", typeof(int));
+            table.Columns.Add("fechaPrestamo", typeof(string));
+            table.Columns.Add("fechaDevolucion", typeof(string));
+
+            foreach (ModelPrestamo prestamo in prestamos)
+            {
+                DataRow row = table.NewRow();
+                row["id"] = prestamo.id;
+                row["libro"] = prestamo.socio;
+                row["libro"] = prestamo.libro;
+                row["fechaPrestamo"] = prestamo.fechaPrestamo;
+                row["fechaDevolucion"] = prestamo.fechaDevolucion;
+                table.Rows.Add(row);
+            }
+            return table;
+        }
+
+        public static void EliminarSocio(int id)
+        {
+            ModelPrestamo prestamo = new ModelPrestamo();
+            prestamo.id = id;
+            prestamo.Delete();
+        }
     }
 }
